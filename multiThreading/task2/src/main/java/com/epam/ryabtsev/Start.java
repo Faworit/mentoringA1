15d75e388a1a739bdcf05374ff5d32b39443bdb3
Sergey_Ryabtsev <Sergey_Ryabtsev@epam.com>
2021-10-24 19:37:44 +0600

    2 work with synchronized block

--- multiThreading/task2/src/main/java/com/epam/ryabtsev/Start.java
+++ multiThreading/task2/src/main/java/com/epam/ryabtsev/Start.java
@@ -1,4 +1,22 @@
 package com.epam.ryabtsev;
 
+import com.epam.ryabtsev.thread.CollectionFiller;
+import com.epam.ryabtsev.thread.SquareRoot;
+import com.epam.ryabtsev.thread.SumNumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Start {
+
+    public static void main(String[] args) {
+        CollectionFiller collectionFiller = new CollectionFiller();
+        SumNumbers sumNumbers = new SumNumbers();
+        SquareRoot squareRoot = new SquareRoot();
+
+        collectionFiller.start();
+        sumNumbers.start();
+        squareRoot.start();
+
+    }
 }
--- multiThreading/task2/src/main/java/com/epam/ryabtsev/thread/SquareRoot.java
+++ multiThreading/task2/src/main/java/com/epam/ryabtsev/thread/SquareRoot.java
@@ -2,19 +2,21 @@ package com.epam.ryabtsev.thread;
 
 import com.epam.ryabtsev.CollectionAction;
 
-public class CollectionFiller extends Thread {
+public class SquareRoot extends Thread {
     CollectionAction collectionAction = new CollectionAction();
 
     public void run() {
         for (int i = 1; i > 0; i++) {
             synchronized (CollectionAction.class) {
-                collectionAction.addNumber();
+                collectionAction.printSquareRoot();
             }
+
             try {
                 Thread.sleep(4);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+
     }
 }
