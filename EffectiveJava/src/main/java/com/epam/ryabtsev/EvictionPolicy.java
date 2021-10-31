e0b6f468009e4b03bd6292aa2a237c2a4d4efa2d
Sergey_Ryabtsev <Sergey_Ryabtsev@epam.com>
2021-10-31 21:00:21 +0600

    4. Average time spent for putting new values into the cache - add to log

--- EffectiveJava/src/main/java/com/epam/ryabtsev/EvictionPolicy.java
+++ EffectiveJava/src/main/java/com/epam/ryabtsev/EvictionPolicy.java
@@ -1,8 +1,24 @@
 package com.epam.ryabtsev;
 
+import com.epam.ryabtsev.service.CacheServiceLFU;
+import org.apache.log4j.Logger;
+
+import java.util.concurrent.TimeUnit;
+
 public class EvictionPolicy {
+    private static Logger logger = Logger.getLogger(EvictionPolicy.class);
 
     public static void LFU() {
+        long start = System.nanoTime();
+        CacheServiceLFU cacheServiceLFU = CacheServiceLFU.init();
+        long end = System.nanoTime();
+        long fillCacheTime = end - start;
+        System.out.println(cacheServiceLFU.getCacheValue().size());
+
+        if (fillCacheTime > 0) {
+            long averageTimeForValue = 100000/(TimeUnit.SECONDS.convert(fillCacheTime, TimeUnit.MILLISECONDS));
+            logger.info("Average time spent for putting new values into the cache: " + averageTimeForValue + " nano seconds");
+        }
 
     }
 
